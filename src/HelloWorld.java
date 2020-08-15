55d3bdf815a3789e16aa2e59ea7260ac7dfe9bde
Aditya Kawale <adityamkawale@gmail.com>
2020-08-15 19:20:20 +0530

    ARS-001 added essential packages - MVC architecture

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,18 @@
 
+
+/*
+ * models package has all the conceptual classes like Customer, Admin,
+ * Flight, Ticket etc.
+ * 
+ * 
+ * services package has all the additional helper classes like MySQL, or
+ * any other indirect classes
+ * 
+ * 
+ * view package will comprise of Java Swing or AWT code for 
+ * User-interfaces
+ * 
+ * */
 public class HelloWorld {
 
 	public static void main(String[] args) {
